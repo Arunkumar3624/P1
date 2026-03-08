4e70fb0e4215d4468a58ef832fc60f24b3c025e8
Arunkumar3624 <arun0362004@gmail.com>
2026-03-08 20:14:22 +0530

    ji

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-const fallbackBaseURL = import.meta.env.PROD
-  ? "https://p2-e3ci.onrender.com/api/v1"
-  : "http://localhost:5000/api/v1";
+const fallbackBaseURL = "https://p2-e3ci.onrender.com/api/v1";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL || fallbackBaseURL,
